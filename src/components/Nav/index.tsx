0551f3da15921980b5304f2c82a22f36588e71aa
82ba10b5e8dc <47543507+82ba10b5e8dc@users.noreply.github.com>
2020-03-11 17:01:12 -0700

    doc: add readme, added extra button to toggle

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -3,13 +3,19 @@ import React, { FC } from 'react'
 import { useNav } from '../../support/use-nav'
 
 const Nav: FC = () => {
-  const [visible] = useNav()
+  const [visible, setVisible] = useNav()
+
+  const toggleNav = () => setVisible(false)
 
   if (!visible) return null
 
   return (
     <div className='nav'>
       Nav is visible!
+
+      <button onClick={toggleNav} type='button'>
+        Close Nav
+      </button>
     </div>
   )
 }
